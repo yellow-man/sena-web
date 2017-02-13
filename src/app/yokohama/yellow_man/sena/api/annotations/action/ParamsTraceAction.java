4e893a8f90379f977558e0a3395a93464133ec3a
yellow-man <yellow-man@users.noreply.github.com>
2017-02-14 01:46:41 +0900

    [タスク]Topページ - スマホだとページング等js周りが機能しない · Issue #5 · yellow-man/sena-web ・datatablesの機能を使ってPOSTしてみたところ、bindFromRequest()でうまいことマッピングできなかった。 　→文字列のキー"columns[8][search][regex]"のような形式で入ってくるためマッピング方法がわからない。問題に遭遇。 【参考資料】 ・ajax https://datatables.net/reference/option/ajax ・Server-side processing https://datatables.net/manual/server-side

--- src/app/yokohama/yellow_man/sena/api/annotations/action/ParamsTraceAction.java
+++ src/app/yokohama/yellow_man/sena/api/annotations/action/ParamsTraceAction.java
@@ -9,6 +9,7 @@ import play.core.j.JavaResultExtractor;
 import play.libs.F.Promise;
 import play.mvc.Action;
 import play.mvc.Http.Context;
+import play.mvc.Http.Request;
 import play.mvc.Result;
 import yokohama.yellow_man.common_tools.CheckUtils;
 import yokohama.yellow_man.sena.core.components.AppLogger;
@@ -23,59 +24,89 @@ public class ParamsTraceAction extends Action<ParamsTraceAction> {
 
 	@Override
 	public Promise<Result> call(Context ctx) throws Throwable {
-		final String prefix = new StringBuffer()
-				.append("[").append(RandomStringUtils.randomAlphanumeric(10)).append("]")
+		Request request = ctx.request();
+
+		final String prefix = new StringBuffer("[" + RandomStringUtils.randomAlphanumeric(20) + "] ")
+				.append("method=").append(request.method())
+				.append(", url=").append(request.secure() ? "https://" : "http://").append(request.host()).append(request.uri())
 				.toString();
+		String startPrefix = "■■■start■■■" + prefix;
 
-		StringBuffer buffRequest = new StringBuffer("■■start■■").append(prefix).append("：[REQUEST PARAMS]");
-
-		Map<String, String[]> headersMap = RequestUtilityComponent.getFromPostMap(ctx.request());
-		if (!CheckUtils.isEmpty(headersMap)) {
-			StringBuffer buffParams = new StringBuffer();
-
-			for (Map.Entry<String, String[]> entry : headersMap.entrySet()) {
-				String key = entry.getKey();
-				String[] values = entry.getValue();
-
-				buffParams.append(System.lineSeparator()).append(key).append(":");
-				if (values != null) {
-					StringBuffer buffValues = new StringBuffer();
-					for (String val : values) {
-						buffValues.append(val).append(", ");
-					}
-					if (buffValues.length() >= 2) {
-						buffValues.delete(buffValues.length() - 2, buffValues.length());
-					}
-					buffParams.append(buffValues);
-				}
-			}
-			buffRequest.append(buffParams).append(System.lineSeparator());
+		Map<String, String[]> map = null;
+		if ("GET".equals(request.method())) {
+			map = RequestUtilityComponent.getQueryString(request);
+		} else {
+			map = RequestUtilityComponent.getFromPostStringMap(request);
 		}
+
+		StringBuffer buffRequest = new StringBuffer(startPrefix)
+				.append("：").append(System.lineSeparator())
+				.append("[REQUEST PARAMS]").append(System.lineSeparator());
+
+		StringBuffer buffParams = new StringBuffer();
+		if (!CheckUtils.isEmpty(map)) {
+			for (Map.Entry<String, String[]> entry : map.entrySet()) {
+				String key = entry.getKey();
+				String[] valArr = entry.getValue();
+
+				buffParams.append(key).append(":");
+				StringBuffer buffValue = new StringBuffer();
+				if (valArr != null) {
+					for (String val : valArr) {
+						buffValue.append(val).append(", ");
+					}
+				}
+				if (buffValue.length() >= 2) {
+					buffValue.delete(buffValue.length() - 2, buffValue.length());
+				}
+				buffParams.append(buffValue);
+				buffParams.append(System.lineSeparator());
+			}
+		}
+		if (buffParams.length() >= System.lineSeparator().length()) {
+			buffParams.delete(buffParams.length() - System.lineSeparator().length(), buffParams.length());
+		}
+
+		buffRequest.append(buffParams).append(System.lineSeparator());
 		AppLogger.info(buffRequest.toString());
 
-		Promise<Result> promise = delegate.call(ctx);
+		try {
+			// メイン処理開始
+			Promise<Result> promise = delegate.call(ctx);
 
-		if (promise != null) {
-			promise.map((result) -> {
-				byte[] body = JavaResultExtractor.getBody(result, 0L);
+			if (promise == null) {
+				AppLogger.info(prefix + " レスポンスデータ取得：promise=null");
+			} else {
+				promise.map((result) -> {
+					byte[] body = JavaResultExtractor.getBody(result, 0L);
 
-				if (body != null) {
-					String strBody = new String(body, "UTF-8");
-					AppLogger.info(new StringBuffer("レスポンスデータ取得：strBody=").append(strBody).toString());
+					if (body == null) {
+						AppLogger.info(prefix + " レスポンスデータ取得：body=null");
+					} else {
+						String strBody = new String(body, "UTF-8");
 
-					JSONObject json = new JSONObject(strBody);
+						try {
+							JSONObject json = new JSONObject(strBody);
+							if (json != null) {
+								strBody = json.toString(2);
+							}
+						} catch (Exception e) {}
 
-					String endPrefix = "■■ end ■■" + prefix;
-					StringBuffer buffResponse = new StringBuffer(endPrefix).append("：[RESPONSE PARAMS]")
-							.append(System.lineSeparator())
-							.append(json.toString(2))
-							.append(System.lineSeparator());
+						String endPrefix = "■■■ end ■■■" + prefix;
+						StringBuffer buffResponse = new StringBuffer(endPrefix)
+								.append("：").append(System.lineSeparator())
+								.append("[RESPONSE PARAMS]").append(System.lineSeparator())
+								.append(strBody).append(System.lineSeparator());
 
-					AppLogger.info(buffResponse.toString());
-				}
-				return result;
-			});
+						AppLogger.info(buffResponse.toString());
+					}
+					return result;
+				});
+			}
+
+			return promise;
+		} catch (Exception e) {
+			throw e;
 		}
-		return promise;
 	}
 }
--- src/app/yokohama/yellow_man/sena/api/controllers/StocksController.java
+++ src/app/yokohama/yellow_man/sena/api/controllers/StocksController.java
@@ -1,5 +1,11 @@
 package yokohama.yellow_man.sena.api.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import play.data.Form;
 import play.mvc.Result;
 import yokohama.yellow_man.sena.api.response.ApiResult;
 
@@ -15,6 +21,54 @@ public class StocksController extends AppWebApiController {
 	public static Result getStocksWithInfoList() {
 		// 返却値初期化
 		ApiResult ret = new ApiResult(API_RES_SUCCESS);
+
+		// パラメータマッピングバリデーションチェック
+		Form<DataParams> requestParams = Form.form(DataParams.class).bindFromRequest();
+		DataParams dataParams = requestParams.get();
+
+		int draw = 1;
+		if (dataParams.draw != null) {
+			draw = Integer.parseInt(dataParams.draw) + 1;
+		}
+
+		// データ
+		Map<String, String> data = new HashMap<String, String>();
+		data.put("first_name", "aaa");
+		data.put("last_name", "bbb");
+		data.put("position", "ccc");
+		data.put("office", "ddd");
+		data.put("start_date", "eee");
+		data.put("salary", "fff");
+		data.put("2first_name", "ggg");
+		data.put("2last_name", "hhh");
+		data.put("2position", "iii");
+		data.put("2office", "jjj");
+		data.put("2start_date", "kkk");
+		data.put("2salary", "lll");
+		data.put("2first_name", "mmm");
+		List<Map<String, String>> dataList = new ArrayList<Map<String, String>>();
+		dataList.add(data);
+
+		// レスポンスセット
+		Map<String, Object> retMap = new HashMap<>();
+		retMap.put("draw", draw);
+		retMap.put("recordsTotal", 1);
+		retMap.put("recordsFiltered", 1);
+		retMap.put("data", dataList);
+		retMap.put("error", "");
+		ret.setContent(retMap);
+
 		return ok(ret.render());
+	}
+
+	public static class DataParams {
+		public String draw;
+// TODO 文字列のキー"columns[8][search][regex]"のような形式で入ってくるためマッピング方法がわからない。。リフレクション使ってマッピングするか？
+//		public List<Map<String, Object>> columns;
+//
+//		public static class Columns {
+//			String name;
+//			Map<String, String> search;
+//		}
 	}
 }
